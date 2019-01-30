49703d0656ea78ae0c59028a330452f0f6246f47
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-01-30 14:57:22 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 01.ReverseStrings

---
+++ 01 - Stacks and Queues/Lab/01.02.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    public class ReverseStrings
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            for (int i = stack.Count - 1; i >= 0; i--)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
