979e4c00d637425c4df8916bcaafaa69f4324c36
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-01-31 12:10:40 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 02.StackSum

---
+++ 01 - Stacks and Queues/Lab/02.01.StackSum/StackSum.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    public class StackSum
+    {
+        public static void Main()
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stackOfNumbers = new Stack<int>();
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                stackOfNumbers.Push(numbersArray[i]);
+            }
+
+            while (true)
+            {
+                string[] line = Console.ReadLine()
+                    .ToLower()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (line[0] == "end")
+                {
+                    break;
+                }
+
+                string command = line[0];
+                int firstNumber = int.Parse(line[1]);
+
+                if (command == "add")
+                {
+                    int secondNumber = int.Parse(line[2]);
+
+                    stackOfNumbers.Push(firstNumber);
+                    stackOfNumbers.Push(secondNumber);
+                }
+                else
+                {
+                    if (stackOfNumbers.Count < firstNumber)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < firstNumber; i++)
+                    {
+                        stackOfNumbers.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum: {stackOfNumbers.Sum()}");
+        }
+    }
+}
---
+++ 01 - Stacks and Queues/Lab/02.02.StackSum/StackSum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    public class StackSum
+    {
+        public static void Main()
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stackOfNumbers = new Stack<int>(numbersArray);
+
+            while (true)
+            {
+                string line = Console.ReadLine().ToLower();
+
+                if (line == "end")
+                {
+                    break;
+                }
+
+                string[] lineParts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string command = lineParts[0];
+                int firstNumber = int.Parse(lineParts[1]);
+
+                if (command == "add")
+                {
+                    int secondNumber = int.Parse(lineParts[2]);
+
+                    stackOfNumbers.Push(firstNumber);
+                    stackOfNumbers.Push(secondNumber);
+                }
+                else
+                {
+                    if (stackOfNumbers.Count < firstNumber)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < firstNumber; i++)
+                    {
+                        stackOfNumbers.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum: {stackOfNumbers.Sum()}");
+        }
+    }
+}
