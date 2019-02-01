69852ea9d9afd254e2a452e71c8271087f592a30
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-01 00:30:54 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 03.SimpleCalculator

---
+++ 01 - Stacks and Queues/Lab/03.01.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                stack.Push(input[i]);
+            }
+
+            while (stack.Count > 1)
+            {
+                int firstOperand = int.Parse(stack.Pop());
+                string operatorSign = stack.Pop();
+                int secondOperand = int.Parse(stack.Pop());
+
+                int result = 0;
+
+                switch (operatorSign)
+                {
+                    case "+":
+                        result = firstOperand + secondOperand;
+                        break;
+
+                    case "-":
+                        result = firstOperand - secondOperand;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                stack.Push(result.ToString());
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ 01 - Stacks and Queues/Lab/03.02.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int firstOperand = int.Parse(stack.Pop());
+                string operatorSign = stack.Pop();
+                int secondOperand = int.Parse(stack.Pop());
+
+                if (operatorSign == "+")
+                {
+                    int result = firstOperand + secondOperand;
+                    stack.Push(result.ToString());
+                }
+                else
+                {
+                    int result = firstOperand - secondOperand;
+                    stack.Push(result.ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
