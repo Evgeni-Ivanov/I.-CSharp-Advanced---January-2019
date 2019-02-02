a70c75d16fe9b2106d061915a0d8dd1c76341778
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-02 18:48:08 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 04.MatchingBrackets

---
+++ 01 - Stacks and Queues/Lab/04.MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    public class MatchingBrackets
+    {
+        public static void Main()
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> openingBracketsIndexes = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openingBracketsIndexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int substringStartIndex = openingBracketsIndexes.Pop();
+                    int substringLength = i - substringStartIndex + 1;
+                    string result = expression.Substring(substringStartIndex, substringLength);
+
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
