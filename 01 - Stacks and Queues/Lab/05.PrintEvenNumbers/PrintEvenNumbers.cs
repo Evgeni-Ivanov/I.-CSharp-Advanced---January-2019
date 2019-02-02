c0ceb963a4c1bb666967a8d69db6613c5c85e7bd
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-02 18:52:00 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 05.PrintEvenNumbers

---
+++ 01 - Stacks and Queues/Lab/05.PrintEvenNumbers/PrintEvenNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues
+{
+    public class PrintEvenNumbers
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    queue.Enqueue(numbers[i]);
+                }
+            }
+
+            while (queue.Count > 1)
+            {
+                Console.Write(queue.Dequeue());
+                Console.Write(", ");
+            }
+
+            if (queue.Peek() % 2 == 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
