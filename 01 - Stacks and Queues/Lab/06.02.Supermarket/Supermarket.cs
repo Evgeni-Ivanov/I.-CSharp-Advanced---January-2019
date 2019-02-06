b57f481c1b4c73d9ccf4738c83aedd3285ee5338
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-06 21:39:54 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 06.Supermarket

---
+++ 01 - Stacks and Queues/Lab/06.02.Supermarket/Supermarket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    public class Supermarket
+    {
+        public static void Main()
+        {
+            Queue<string> people = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "End":
+                        Console.WriteLine($"{people.Count} people remaining.");
+                        return;
+
+                    case "Paid":
+                        int n = people.Count;
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.WriteLine(people.Dequeue());
+                        }
+                        break;
+
+                    default:
+                        people.Enqueue(input);
+                        break;
+                }
+            }
+        }
+    }
+}
