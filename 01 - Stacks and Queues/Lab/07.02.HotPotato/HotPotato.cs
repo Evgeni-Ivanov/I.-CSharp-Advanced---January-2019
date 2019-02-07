40d2932ed6c94603e036478492f97858178518ca
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-07 22:08:23 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 07.HotPotato

---
+++ 01 - Stacks and Queues/Lab/07.02.HotPotato/HotPotato.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    public class HotPotato
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int fatalToss = int.Parse(Console.ReadLine());
+
+            Queue<string> players = new Queue<string>(input);
+
+            int counter = 1;
+
+            while (players.Count > 1)
+            {
+                string player = players.Dequeue();
+
+                if (counter == fatalToss)
+                {
+                    Console.WriteLine($"Removed {player}");
+
+                    counter = 1;
+                }
+                else
+                {
+                    players.Enqueue(player);
+
+                    counter++;
+                }
+            }
+
+            string lastPlayer = players.Dequeue();
+
+            Console.WriteLine($"Last is {lastPlayer}");
+        }
+    }
+}
