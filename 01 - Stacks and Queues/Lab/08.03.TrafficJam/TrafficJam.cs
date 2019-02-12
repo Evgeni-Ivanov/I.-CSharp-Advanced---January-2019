3280def8179ade8dc28206608dcc910b2ec7b7a4
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-12 23:36:28 +0000

    Add 01 - Stacks and Queues - Lab
    Add Problem 08.TrafficJam

---
+++ 01 - Stacks and Queues/Lab/08.03.TrafficJam/TrafficJam.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueues
+{
+    public class TrafficJam
+    {
+        public static void Main()
+        {
+            // n - Number of cars that can pass during a green light
+            int n = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+
+            Queue<string> carsInTraffic = new Queue<string>();
+
+            int totalNumberOfPassedCars = 0;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    int passedCars = Math.Min(n, carsInTraffic.Count);
+
+                    for (int i = 0; i < passedCars; i++)
+                    {
+                        string carThatPassed = carsInTraffic.Dequeue();
+                        Console.WriteLine($"{carThatPassed} passed!");
+
+                        totalNumberOfPassedCars++;
+                    }
+                }
+                else
+                {
+                    carsInTraffic.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{totalNumberOfPassedCars} cars passed the crossroads.");
+        }
+    }
+}
