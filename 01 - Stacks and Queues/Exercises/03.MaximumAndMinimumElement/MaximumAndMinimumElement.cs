d524ecd3a2bbd12078320e94d55621da8880d41a
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-24 23:40:07 +0000

    Add Problem 03.MaximumAndMinimumElement
    Add 01 - Stacks and Queues - Exercises: Problem 03.MaximumAndMinimumElement

---
+++ 01 - Stacks and Queues/Exercises/03.MaximumAndMinimumElement/MaximumAndMinimumElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture01StacksAndQueues
+        .Exercises
+        .Problem03MaximumAndMinimumElement
+{
+    public class MaximumAndMinimumElement
+    {
+        public static void Main()
+        {
+            Stack<int> stackOfNumbers = new Stack<int>();
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] commands = Console.ReadLine().Split();
+
+                int command = int.Parse(commands[0]);
+
+                if (command == 1)
+                {
+                    int numberToPush = int.Parse(commands[1]);
+
+                    stackOfNumbers.Push(numberToPush);
+                }
+                else if (command == 2 && stackOfNumbers.Any())
+                {
+                    stackOfNumbers.Pop();
+                }
+                else if (command == 3 && stackOfNumbers.Any())
+                {
+                    int maximumElement = stackOfNumbers.Max();
+                    Console.WriteLine(maximumElement);
+                }
+                else if (command == 4 && stackOfNumbers.Any())
+                {
+                    int minimumElement = stackOfNumbers.Min();
+                    Console.WriteLine(minimumElement);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", stackOfNumbers));
+        }
+    }
+}
