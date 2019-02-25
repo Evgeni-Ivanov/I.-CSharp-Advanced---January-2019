888036193fea0f756b0c06f6ab4816d6dbd79804
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-25 23:07:25 +0000

    Add Problem 05.FashionBoutique
    Add 01 - Stacks And Queues - Exercises: Problem 05.FashionBoutique

---
+++ 01 - Stacks and Queues/Exercises/05.FashionBoutique/FashionBoutique.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture01StacksAndQueues
+        .Exercises
+        .Problem05FashionBoutique
+{
+    public class FashionBoutique
+    {
+        public static void Main()
+        {
+            int[] sequenceOfClothes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            Stack<int> clothes = new Stack<int>(sequenceOfClothes);
+
+            int numberOfRacks = 1;
+            int currentRackCapacity = rackCapacity;
+
+            while (clothes.Count > 0)
+            {
+                int peekCloth = clothes.Peek();
+
+                if (currentRackCapacity >= peekCloth)
+                {
+                    int currentCloth = clothes.Pop();
+
+                    currentRackCapacity -= currentCloth;
+                }
+                else
+                {
+                    currentRackCapacity = rackCapacity;
+                    numberOfRacks++;
+                }
+            }
+
+            Console.WriteLine(numberOfRacks);
+        }
+    }
+}
