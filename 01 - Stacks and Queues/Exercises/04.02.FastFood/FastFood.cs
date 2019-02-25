c59d8dd301bf6fa6c01879daccadc26739fe975a
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-25 23:06:21 +0000

    Add Problem 04.02.FastFood
    Add 01 - Stacks And Queues - Exercises: Problem 04.02.FastFood

---
+++ 01 - Stacks and Queues/Exercises/04.02.FastFood/FastFood.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture01StacksAndQueues
+        .Exercises
+        .Problem04v02FastFood
+{
+    public class FastFood
+    {
+        public static void Main()
+        {
+            int totalFoodQuantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queueOfOrders = new Queue<int>(orders);
+
+            int biggestOrder = queueOfOrders.Max();
+
+            int countOfQueueOfOrders = queueOfOrders.Count;
+
+            for (int i = 0; i < countOfQueueOfOrders; i++)
+            {
+                int peekOrder = queueOfOrders.Peek();
+
+                if (totalFoodQuantity >= peekOrder)
+                {
+                    int currentOrder = queueOfOrders.Dequeue();
+
+                    totalFoodQuantity -= currentOrder;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(biggestOrder);
+
+            if (queueOfOrders.Count < 1)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {String.Join(" ", queueOfOrders)}");
+            }
+        }
+    }
+}
