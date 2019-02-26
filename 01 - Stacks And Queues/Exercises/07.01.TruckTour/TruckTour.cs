f78a966df039f8cc1544012b2226e57b898a0825
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-02-26 23:58:04 +0000

    Add Problem 07.01.TruckTour
    Add 01 - Stacks And Queues - Exercises: Problem 07.01.TruckTour

---
+++ 01 - Stacks And Queues/Exercises/07.01.TruckTour/TruckTour.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture01StacksAndQueues
+        .Exercises
+        .Problem07v01TruckTour
+{
+    public class TruckTour
+    {
+        public static void Main()
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+
+            Queue<int[]> queueOfPumps = new Queue<int[]>();
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                queueOfPumps.Enqueue(input);
+            }
+
+            bool fullCircle = true;
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                int totalFuel = 0;
+
+                for (int j = 0; j < numberOfPumps; j++)
+                {
+                    int[] currentPump = queueOfPumps.Dequeue();
+
+                    int amountOfPetrol = currentPump[0];
+                    int distanceToNextPetrolPump = currentPump[1];
+
+                    totalFuel += amountOfPetrol - distanceToNextPetrolPump;
+
+                    if (totalFuel < 0)
+                    {
+                        fullCircle = false;
+                    }
+
+                    queueOfPumps.Enqueue(currentPump);
+                }
+
+                if (fullCircle == true)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+                int[] newStartingPump = queueOfPumps.Dequeue();
+                queueOfPumps.Enqueue(newStartingPump);
+                fullCircle = true;
+            }
+        }
+    }
+}
