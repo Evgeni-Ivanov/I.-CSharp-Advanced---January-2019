52a796793ad08e83dec95762529abaa739f25e92
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-03 22:37:21 +0000

    Add Problem 03.PrimaryDiagonal
    Add lecture 02 - Multidimensional Arrays - Lab: Problem 03.PrimaryDiagonal

---
+++ 02 - Multidimensional Arrays/Lab/03.PrimaryDiagonal/PrimaryDiagonal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture02MultidimensionalArrays
+        .Lab
+        .Problem03PrimaryDiagonal
+{
+    public class PrimaryDiagonal
+    {
+        public static void Main()
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfMatrix, sizeOfMatrix];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = columnElements[column];
+                }
+            }
+
+            int sumOfMatrixPrimaryDiagonal = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sumOfMatrixPrimaryDiagonal += matrix[i, i];
+            }
+
+            Console.WriteLine(sumOfMatrixPrimaryDiagonal);
+        }
+    }
+}
