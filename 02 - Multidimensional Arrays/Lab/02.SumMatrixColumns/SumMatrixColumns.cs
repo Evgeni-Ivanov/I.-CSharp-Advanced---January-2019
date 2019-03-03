4d5345b081164bccdf9305f1da477272c1220e18
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-03 22:36:37 +0000

    Add Problem 02.SumMatrixColumns
    Add lecture 02 - Multidimensional Arrays - Lab: Problem 02.SumMatrixColumns

---
+++ 02 - Multidimensional Arrays/Lab/02.SumMatrixColumns/SumMatrixColumns.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture02MultidimensionalArrays
+        .Lab
+        .Problem02SumMatrixColumns
+{
+    public class SumMatrixColumns
+    {
+        public static void Main()
+        {
+            int[] sizesOfMatrix = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[sizesOfMatrix[0], sizesOfMatrix[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = columnElements[column];
+                }
+            }
+
+            for (int column = 0; column < matrix.GetLength(1); column++)
+            {
+                int columnSum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    columnSum += matrix[row, column];
+                }
+
+                Console.WriteLine(columnSum);
+            }
+        }
+    }
+}
