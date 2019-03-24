fad48885da1939041625e8902be98ef5f27ab476
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-24 23:24:49 +0000

    Add Problem 01.01.CountSameValuesInArray
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 01.01.CountSameValuesInArray

---
+++ 03 - Sets And Dictionaries Advanced/Lab/01.01.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Lab
+        .Problem01v01CountSameValuesInArray
+{
+    public class CountSameValuesInArray
+    {
+        public static void Main()
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> numberOfOccurrences = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numberOfOccurrences.ContainsKey(numbers[i]) == false)
+                {
+                    numberOfOccurrences.Add(numbers[i], 1);
+                }
+                else
+                {
+                    numberOfOccurrences[numbers[i]]++;
+                }
+            }
+
+            foreach (var number in numberOfOccurrences)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
