5aafe014aa7a96550f3ef5ca279379d37e9c7c07
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-27 00:52:47 +0000

    Add Problem 03.ProductShop
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 03.ProductShop

---
+++ 03 - Sets And Dictionaries Advanced/Lab/03.ProductShop/ProductShop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Lab
+        .Problem03ProductShop
+{
+    public class ProductShop
+    {
+        public static void Main()
+        {
+            SortedDictionary<string, Dictionary<string, double>> shopsDictionary =
+                new SortedDictionary<string, Dictionary<string, double>>();
+
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "Revision")
+            {
+                string shop = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (shopsDictionary.ContainsKey(shop) == false)
+                {
+                    shopsDictionary.Add(shop, new Dictionary<string, double>());
+
+                    if (shopsDictionary[shop].ContainsKey(product) == false)
+                    {
+                        shopsDictionary[shop].Add(product, price);
+                    }
+                }
+                else
+                {
+                    shopsDictionary[shop].Add(product, price);
+                }
+
+                input = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var currentShop in shopsDictionary)
+            {
+                Console.WriteLine($"{currentShop.Key}->");
+
+                foreach (var currentProduct in currentShop.Value)
+                {
+                    Console.WriteLine($"Product: {currentProduct.Key}, Price: {currentProduct.Value}");
+                }
+            }
+        }
+    }
+}
