f706cfa4dafc38abd0f66dcb3d0794d0b627d356
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-29 01:01:50 +0000

    Add Problem 05.RecordUniqueNames
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 05.RecordUniqueNames

---
+++ 03 - Sets And Dictionaries Advanced/Lab/05.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Lab
+        .Problem05RecordUniqueNames
+{
+    public class RecordUniqueNames
+    {
+        public static void Main()
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var currentName in names)
+            {
+                Console.WriteLine(currentName);
+            }
+        }
+    }
+}
