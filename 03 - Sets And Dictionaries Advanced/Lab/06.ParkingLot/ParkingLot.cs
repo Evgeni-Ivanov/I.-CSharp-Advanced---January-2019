9d5f1bd50e544db4becf7aa737cef4196504de93
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-30 00:49:32 +0000

    Add Problem 06.ParkingLot
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 06.ParkingLot

---
+++ 03 - Sets And Dictionaries Advanced/Lab/06.ParkingLot/ParkingLot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Lab
+        .Problem06ParkingLot
+{
+    public class ParkingLot
+    {
+        public static void Main()
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "END")
+            {
+                string command = input[0];
+                string carNumber = input[1];
+
+                if (input[0] == "IN")
+                {
+                    carNumbers.Add(carNumber);
+                }
+                else
+                {
+                    carNumbers.Remove(carNumber);
+                }
+
+                input = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (carNumbers.Count > 0)
+            {
+                foreach (var currentCarNumber in carNumbers)
+                {
+                    Console.WriteLine(currentCarNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
