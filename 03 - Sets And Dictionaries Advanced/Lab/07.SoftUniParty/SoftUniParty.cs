a4e7f7c826f57b8e8be09cb13c93f7c8fd16a160
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-03-31 00:54:19 +0000

    Add Problem 07.SoftUniParty
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 07.SoftUniParty

---
+++ 03 - Sets And Dictionaries Advanced/Lab/07.SoftUniParty/SoftUniParty.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Lab
+        .Problem07SoftUniParty
+{
+    public class SoftUniParty
+    {
+        public static void Main()
+        {
+            HashSet<string> regularGuests = new HashSet<string>();
+            HashSet<string> vipGuests = new HashSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "PARTY")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Remove(input);
+                }
+                else
+                {
+                    regularGuests.Remove(input);
+                }
+            }
+
+            Console.WriteLine($"{vipGuests.Count + regularGuests.Count}");
+
+            foreach (var vipGuest in vipGuests)
+            {
+                Console.WriteLine(vipGuest);
+            }
+
+            foreach (var regularGuest in regularGuests)
+            {
+                Console.WriteLine(regularGuest);
+            }
+        }
+    }
+}
