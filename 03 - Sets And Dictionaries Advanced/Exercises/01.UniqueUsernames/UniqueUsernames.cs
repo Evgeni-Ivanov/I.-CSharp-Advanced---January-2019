a7a663443d2fa74525b147dd3e567b6459788121
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-04-01 14:04:21 +0100

    Add Problem 01.UniqueUsernames
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 01.UniqueUsernames

---
+++ 03 - Sets And Dictionaries Advanced/Exercises/01.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Exercises
+        .Problem01UniqueUsernames
+{
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            int usernamesCount = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < usernamesCount; i++)
+            {
+                string username = Console.ReadLine();
+
+                usernames.Add(username);
+            }
+
+            foreach (var currentUsername in usernames)
+            {
+                Console.WriteLine(currentUsername);
+            }
+        }
+    }
+}
