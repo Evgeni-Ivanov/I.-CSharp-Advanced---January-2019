ec077996c3b9ffb6313a05588184f90a99234f7b
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-04-03 14:42:04 +0100

    Add Problem 02.02.SetsOfElements
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 02.02.SetsOfElements

---
+++ 03 - Sets And Dictionaries Advanced/Exercises/02.02.SetsOfElements/SetsOfElements.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Exercises
+        .Problem02v02SetsOfElements
+{
+    public class SetsOfElements
+    {
+        public static void Main()
+        {
+            int[] setsLengths = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstSetLength = setsLengths[0];
+            int secondSetLength = setsLengths[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < firstSetLength; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                firstSet.Add(number);
+            }
+
+            for (int i = 0; i < secondSetLength; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                secondSet.Add(number);
+            }
+
+            firstSet.IntersectWith(secondSet);
+            Console.WriteLine($"{string.Join(' ', firstSet)}");
+
+            // Or:
+            // Console.WriteLine($"{string.Join(' ', firstSet.Intersect(secondSet))}");
+        }
+    }
+}
