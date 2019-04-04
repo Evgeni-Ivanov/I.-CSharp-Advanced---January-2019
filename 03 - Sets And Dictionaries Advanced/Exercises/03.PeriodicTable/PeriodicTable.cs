b56d47658601704fbf90bf0c0727985b9a746411
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-04-04 12:12:24 +0100

    Add Problem 03.PeriodicTable
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 03.PeriodicTable

---
+++ 03 - Sets And Dictionaries Advanced/Exercises/03.PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Exercises
+        .Problem03PeriodicTable
+{
+    public class PeriodicTable
+    {
+        public static void Main()
+        {
+            int linesOfChemicalCompoundsCount = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalCompounds = new SortedSet<string>();
+
+            for (int currentLine = 0; currentLine < linesOfChemicalCompoundsCount; currentLine++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int currentElement = 0; currentElement < elements.Length; currentElement++)
+                {
+                    chemicalCompounds.Add(elements[currentElement]);
+                }
+            }
+
+            foreach (var currentChemicalCompound in chemicalCompounds)
+            {
+                Console.Write($"{string.Join(' ', currentChemicalCompound)} ");
+            }
+        }
+    }
+}
