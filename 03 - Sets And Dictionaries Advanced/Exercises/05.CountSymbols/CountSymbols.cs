fed5e0e8f3a56553b6c6626092467242d54edf3c
Evgeni Ivanov <evgeniivanov.bg@gmail.com>
2019-04-07 19:07:26 +0100

    Add Problem 05.CountSymbols
    Add lecture 03 - Sets And Dictionaries Advanced - Lab: Problem 05.CountSymbols

---
+++ 03 - Sets And Dictionaries Advanced/Exercises/05.CountSymbols/CountSymbols.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni
+        .ProfessionalModules
+        .CSharpWebDeveloper
+        .CSharpFundamentals
+        .CSharpFundamentalsJanuary2019
+        .CSharpAdvancedJanuary2019
+        .Lecture03SetsAndDictionariesAdvanced
+        .Exercises
+        .Problem05CountSymbols
+{
+    public class CountSymbols
+    {
+        public static void Main()
+        {
+            char[] someText = Console.ReadLine().ToCharArray();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < someText.Length; i++)
+            {
+                char symbol = someText[i];
+
+                if (symbols.ContainsKey(symbol) == false)
+                {
+                    symbols.Add(symbol, 0);
+                }
+
+                symbols[symbol]++;
+            }
+
+            foreach (var currentSymbol in symbols)
+            {
+                Console.WriteLine($"{currentSymbol.Key}: {currentSymbol.Value} time/s");
+            }
+        }
+    }
+}
